2911ff8733d51acf5014bd147de4d09e0d691ca2
abigiyaDa <abigiyaDa@gmail.com>
2025-06-24 23:34:34 +0400

    top k frequent element

--- TopKFrequent.py
+++ TopKFrequent.py
@@ -1,15 +1,17 @@
+import heapq
+from typing import List
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        count ={}
-        freq = [[] for i in range(len(nums)+1)]        
-
-        for n in nums:
-            count[n] = count.get(n,0)+1
-        for n, c in count.items():
-            freq[c].append(n)
-        res = []
-        for i in range(len(freq)-1,0,-1):
-            for n in freq[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
+        freq = {}
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+        
+        # Use a heap of size k to keep top k elements
+        heap = []
+        for num, count in freq.items():
+            heapq.heappush(heap, (count, num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+        
+        return [num for count, num in heap]
