a189c4ee64c61059a87c7ccdc65afa36a2e6d9a9
ABIGIYA DANIEL <abigiyada@gmail.com>
2025-07-02 18:43:24 +0400

    Time: 0 ms (100.00%) | Memory: 54 MB (19.77%) - LeetSync

---
+++ 27-remove-element/remove-element.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    var numberOfElements = nums.length 
+
+    for (var i = 0 ; i< nums.length; i++){
+        if(nums[i] === val){
+            nums.splice(i,1)
+            i--;
+            numberOfElements--;
+        }
+    }
+    return numberOfElements
+    
+};
