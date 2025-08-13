02a76ab7b1a5bc3d3f439a57123ae6565b9b5da5
ABIGIYA DANIEL <abigiyada@gmail.com>
2025-08-13 13:24:26 +0300

    Time: 646 ms (5.04%) | Memory: 18.3 MB (48.68%) - LeetSync

---
+++ 2502-sort-the-people/sort-the-people.py
@@ -0,0 +1,24 @@
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        # # using insertion sorting 
+        # n=len(names)
+        # for i in range(1,n):
+        #     key_height = heights[i]
+        #     key_name = names[i]
+        #     j=i-1
+        #     while j>=0 and heights[j]<key_height:
+        #         heights[j+1] = heights[j]
+        #         names[j+1]=names[j]
+        #         j-=1#important 
+        #     heights[j+1]=key_height
+        #     names[j+1]=key_name    
+        # return names
+
+        # using bubble sort
+        n = len(names)
+        for i in range(n):
+            for j in range(1,n-i):
+                if heights[j] > heights[j-1]:
+                    heights[j] , heights[j-1] = heights[j-1],heights[j]
+                    names[j],names[j-1] = names[j-1],names[j]
+        return names
