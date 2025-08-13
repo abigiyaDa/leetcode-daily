95d0c5e7dcc831b569e206853e2d4fe713fd30f6
ABIGIYA DANIEL <abigiyada@gmail.com>
2025-08-13 13:56:50 +0300

    Time: 229 ms (10.13%) | Memory: 18.1 MB (93.71%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -14,11 +14,23 @@ class Solution:
         #     names[j+1]=key_name    
         # return names
 
-        # using bubble sort
+        # # using bubble sort
+        # n = len(names)
+        # for i in range(n):
+        #     for j in range(1,n-i):
+        #         if heights[j] > heights[j-1]:
+        #             heights[j] , heights[j-1] = heights[j-1],heights[j]
+        #             names[j],names[j-1] = names[j-1],names[j]
+        # return names
+
+        # using selection sorting 
         n = len(names)
         for i in range(n):
-            for j in range(1,n-i):
-                if heights[j] > heights[j-1]:
-                    heights[j] , heights[j-1] = heights[j-1],heights[j]
-                    names[j],names[j-1] = names[j-1],names[j]
+            max_index = i
+            for j in range(i+1,n):
+                if heights[j] > heights[max_index]:
+                    max_index = j
+            heights[i] , heights[max_index] = heights[max_index], heights[i]
+            names[i],names[max_index] = names[max_index],names[i]    
         return names
+
