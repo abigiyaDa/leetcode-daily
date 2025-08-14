3c24a91087ed3d3f02123c903d2f6309c2dd4159
ABIGIYA DANIEL <abigiyada@gmail.com>
2025-08-14 09:57:08 +0300

    Time: 7 ms (42.53%) | Memory: 17.8 MB (44.41%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -10,10 +10,19 @@ class Solution:
             5 : "V",   4 : "IV",
             1 : "I", 
         }
+        #       #first and easy approach 
+        # res = []
+        # for i in romans:
+        #     sym = romans[i]
+        #     while num >= i:
+        #         num -= i
+        #         res.append(sym)
+        # return ''.join(res)
+
         res = []
-        for i in romans:
-            values = romans[i]
-            while num >= i:
-                num -= i
-                res.append(values)
+        for key,sym in romans.items():
+            if num // key:
+                count = num // key
+                res.append(sym*count)
+                num = num%key
         return ''.join(res)
