ce24c070620b227485005861ec3d685f9a3e4f66
ABIGIYA DANIEL <abigiyada@gmail.com>
2026-03-06 09:43:03 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (69.48%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,33 +1,11 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        res = ""
-        for i in range(len(strs[0])):
+        shortest_string = sorted(strs, key=len)[0]
+
+        for j in range(len(shortest_string)):
             for s in strs:
-                #if strs[0][i] this specific letter must equals s[i] if so continue else return res
-                # what if s[i] is out of bound check if i == len(s)
-                if i == len(s) or s[i] != strs[0][i]:
-                    return res
-            res+= strs[0][i]
-        return res
-
-
-        # a = strs[0]  
-        # temp = ""    
-
-        # for i in range(len(a)):
-        #     current_char = a[i]  
-        #     match = True
-        #     for j in range(1, len(strs)):
-        #         if i >= len(strs[j]) or strs[j][i] != current_char:
-        #             match = False
-        #             break
-
-        #     if match:
-        #         temp += current_char
-        #     else:
-        #         break  
-
-        # return temp
-
-
+                if s[j] != shortest_string[j]:
+                    return shortest_string[:j]
+                
+        return shortest_string
         
