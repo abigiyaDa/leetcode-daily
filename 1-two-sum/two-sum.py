9bdc37d473fdd7251042587f3cba9b5dfba82e48
ABIGIYA DANIEL <abigiyada@gmail.com>
2026-03-27 10:26:00 +0300

    Time: 0 ms (100.00%) | Memory: 20.4 MB (63.82%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,32 +1,10 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        #optimal solution
-        # x = {}
-        # for i,v in enumerate(nums):
-        #     if(target - v in x):
-        #         return [x[target - v],i]
-        #     else:
-        #         x[v] = i
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        checker = {}
+        for index , num in enumerate(nums):
+            complement = target - num
+            if complement in checker:
+                return [checker[complement],index]
+            checker[num] = index
         
-        #creates pairs of (value, original_index): 
-        # space 0(n)
-
-        arr = [(num, i) for i, num in enumerate(nums)]
-        arr.sort()  
-        left = 0
-        right = len(nums)-1
-        while left < right:
-            current_sum = arr[left][0]+arr[right][0]
-            if current_sum == target:
-                return [arr[left][1],arr[right][1]]
-            elif current_sum < target :
-                left +=1
-            else:
-                right -= 1
-            
-            
-        
-            
-        
-
         
