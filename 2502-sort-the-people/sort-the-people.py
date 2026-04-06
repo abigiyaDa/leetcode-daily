03bbe6cb334e58038fee8a206fee8505c61a2fbd
ABIGIYA DANIEL <abigiyada@gmail.com>
2026-04-06 12:25:12 +0300

    Time: 353 ms (18.72%) | Memory: 19.6 MB (90.27%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -1,12 +1,35 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        # bubble sorting time o(n^2) space 0(1)
+        # insertion sorting 
         size = len(names)
-        for i in range(size):
-            for j in range(size - i-1):
-                if heights[j] < heights[j+1]:
-                    heights[j], heights[j+1] = heights[j+1], heights[j]
-                    names[j],names[j+1] = names[j+1] , names[j]
+        for i in range(1,size):
+            key_h = heights[i]
+            key_n = names[i]
+            j= i-1
+            while j >= 0 and key_h > heights[j]:
+                heights[j + 1] = heights[j]
+                names[j + 1] = names[j] 
+                j -= 1
+            names[j+1] = key_n
+            heights[j+1] = key_h
         return names
+        # # selection sorting time o(n^2) space 0(1)
+        # size = len(names)
+        # for i in range(size):
+        #     min_idx = i
+        #     for j in range(i+1,size):
+        #         if heights[min_idx] < heights[j]:
+        #             min_idx = j
+        #     heights[i] ,heights[min_idx]= heights[min_idx], heights[i]
+        #     names[i],names[min_idx] = names[min_idx] , names[i]
+        # return names
+        # # bubble sorting time o(n^2) space 0(1)
+        # size = len(names)
+        # for i in range(size):
+        #     for j in range(size - i-1):
+        #         if heights[j] < heights[j+1]:
+        #             heights[j], heights[j+1] = heights[j+1], heights[j]
+        #             names[j],names[j+1] = names[j+1] , names[j]
+        # return names
 
         
