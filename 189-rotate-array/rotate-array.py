ba46c8aed1a17b91fb40796f554b68a25d9a41a1
ABIGIYA DANIEL <abigiyada@gmail.com>
2026-04-14 14:44:39 +0300

    Time: 9 ms (26.47%) | Memory: 26.9 MB (17.91%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -1,26 +1,43 @@
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        #soln 2 good time o(n) and space o(1)
-        # normalize k, for k > len(nums) -otherwise, it will break
-        k %= len(nums)
-        def reverse(start,end):
-            while start<end:
-                nums[start],nums[end] = nums[end],nums[start]
-                start +=1
-                end -=1
-        reverse(0,len(nums)-1)
-        reverse(0,k-1)
-        reverse(k,len(nums)-1)
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # reverse the whole then reverse the 1st k then reverse the rest 
+        # nums = [1,2,3,4,5,6,7], k = 3
+        # Step 1: reverse all
+        # → [7,6,5,4,3,2,1]
+        # Step 2: reverse first k
+        # → [5,6,7,4,3,2,1]
+        # Step 3: reverse rest
+        # → [5,6,7,1,2,3,4]
 
-        #solution 1 but time complexity is o(n+k) and not scalable 
-        # count = 0
-        # lastIndex = len(nums)-1
-        # while count < k:
-        #     nums.insert(0,nums[lastIndex])
-        #     count +=1
-        #     nums.pop()
-        # return nums 
-        
+        # option 1 
 
-        
-        
+        # time O(n) optimal in-place solution
+        m = len(nums)
+        k = k % m # bc k is the same as k % m and if k > m then code breaks 
+
+        def to_rev(left,right):
+            # reverse 
+            while left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+        # reverse the whole list
+        to_rev(0,m-1)
+        # reverse the 1st k
+        to_rev(0,k-1)
+        # reverse the rest after k
+        to_rev(k,m-1)
+
+        # option 2
+
+        # time O(n × k) n- for the slicing
+        # m = len(nums)
+        # k = k % m # bc k is the same as k % m
+        # for i in range(k):
+        #     last = nums[-1]
+        #     # shift
+        #     nums[1:m] = nums[:-1]
+        #     nums[0] = last
