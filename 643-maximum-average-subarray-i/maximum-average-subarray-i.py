8f2520f235b6235e23921915cc8acce9847abec8
ABIGIYA DANIEL <abigiyada@gmail.com>
2026-04-20 10:31:21 +0300

    Time: 64 ms (52.80%) | Memory: 28.9 MB (79.82%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -1,30 +1,16 @@
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        # n=len(nums)
-        # window_sum = sum(nums[:k])
-        # max_sum = window_sum
-        # for i in range(n-k):
-        #     window_sum = window_sum - nums[i] + nums[i+k]
-        #     max_sum = max(window_sum , max_sum)
-        # output = (max_sum / k) * 1.00000
-        # return output
-
-        window_sum = 0
-        max_sum = float('-inf')
-        # float('-inf') is the safest default starting point when looking for a maximum of unknown numbers. started with max_sum = 0 and num = [-5, -2, -9] but the max num is -2 not 0(our default)
-
-        for i in range(k):
-            window_sum += nums[i]
+        n = len(nums)-1
+        cur_sum = sum(nums[:k])
+        max_avg = cur_sum/k
+        start = 0
+        end = k-1
+        while end < n:
+            cur_sum -= nums[start]
+            start+=1
+            end+=1
+            cur_sum += nums[end]
+            if (cur_sum/k) > max_avg:
+                max_avg = (cur_sum/k)
+        return max_avg
         
-        max_sum = window_sum
-        left = 0
-
-        for right in range(k,len(nums)):
-            window_sum += nums[right]
-            window_sum -= nums[left]
-
-            max_sum = max(max_sum,window_sum)
-            left+=1
-
-        return max_sum/k
-
